8c9c2a3bf36fd917f85198a4da6094f064497250
ferrisbueller66 <github email address>
2020-05-24 14:02:59 -0400

    Done.

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,16 @@
 class Item
   attr_accessor :name, :price
 
+  @@all = []  #added
+
   def initialize(name,price)
     @name = name
     @price = price
+    @@all << self  #added
   end
+
+  def self.all  #added
+    @@all
+  end
+
 end
